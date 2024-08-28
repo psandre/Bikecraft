1fbf5652c4fbcdaf4a593a25c54348139b7b37fb
Andre Souza <souzzavix@gmail.com>
2024-08-28 10:58:14 -0300

    Subindo Projeto ao git

---
+++ mobile-navbar.js
@@ -0,0 +1,19 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function ToggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefaut()
+
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList. contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active){
+  event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+  } 
+  else{
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', ToggleMenu);
+btnMobile.addEventListener('touchstart', ToggleMenu);
